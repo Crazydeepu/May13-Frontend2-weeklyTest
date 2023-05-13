dfe9a068175dd08041d1c221ec4e36d6f3584285
Anudeep_Reddy <anudeepreddypocha@gmail.com>
2023-05-13 21:42:12 +0530

    Initial Commit

--- script.js
+++ script.js
@@ -8,20 +8,49 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  arr.map(arrMapping);
+
+  function arrMapping(list) {
+    if(list.marks >= 50) {
+      console.log(list);
+    }
+  }
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(forEachMethod);
+
+  function forEachMethod(list1) {
+    if(list1.marks >= 50) {
+      console.log(list1);
+    }
+  }
 }
 
 function addData() {
   //Write your code here, just console.log
+  let obj1 = {id:4,name:"susan",age:"20",marks:45};
+  arr.push(obj1);
+  console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  for(let i=0;i<arr.length;i++){
+    if(arr[i].marks < 50) {
+      arr.splice(i,2);
+    }
+  }
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let newArr = [
+    {id:5,name:'Hofstader',age:'24',marks:92},
+    {id:6,name:'Cooper',age:'21',marks:80},
+    {id:7,name:'Kothrapali',age:'20',marks:90}
+  ]
+  console.log(arr.concat(newArr));
 }
